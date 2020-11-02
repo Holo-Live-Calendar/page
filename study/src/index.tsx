fb67bb91907ffd19f4d0a37ba901df1bdc46c191
Yuto Watanabe <yuto.w51942@gmail.com>
2020-11-02 16:16:41 +0900

    yatta

--- study/src/index.tsx
+++ study/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 
 interface State {
   date: Date
+  isToggleOn: boolean
 }
 
 class Clock extends React.Component<{}, State> {
@@ -11,7 +12,12 @@ class Clock extends React.Component<{}, State> {
 
   constructor(props: {}){
     super(props)
-    this.state = {date: new Date()}
+    this.state = {
+      date: new Date(),
+      isToggleOn: true
+    }
+
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
@@ -27,6 +33,12 @@ class Clock extends React.Component<{}, State> {
     }
   }
 
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }))
+  }
+
   tick() {
     this.setState({
       date: new Date()
@@ -38,6 +50,9 @@ class Clock extends React.Component<{}, State> {
       <div>
         <h1>Hello World!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
       </div>
     )
   }
